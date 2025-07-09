851f9db9ebc05d94a25fef76e470a3f6ce0e62de
Dsantos80 <davidsantosbelmonte@gmail.com>
2025-07-09 20:55:25 +0200

    Correcciones menores. Añadido logo a la factura

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,29 +1,29 @@
 let productosDisponibles = [];
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-let metodoEnvio = 'domicilio'; // Valor por defecto
+let metodoEnvio = 'domicilio'; //Iniciamos en domicilio
 
 let descuentoAplicado = 0; // Porcentaje de descuento
 
+//Si no hay pedidos, el botón de pago se desactiva
 document.addEventListener('DOMContentLoaded', function () {
   actualizarEstadoBotonCheckout();
 });
-
+//Función que muestra el resumen del pedido
 function mostrarResumenPedido() {
   const $resumen = $('#resumen-pedido');
+  //Limpiamos el resumen cuando llamamos a esta función
   $resumen.empty();
-
+  //Si el carrito está vacío, mostramos el mensaje de carrito vacío y actualizamos gastos y total
   if (carrito.length === 0) {
     mostrarMensajeCarritoVacio();
     actualizarGastosYTotal();
     return;
   }
-
+  //Creamos un elemento de la lista con cada elemento del carrito
   carrito.forEach(item => {
     const producto = productosDisponibles.find(p => p.id === item.id);
     if (!producto) return;
-
     const subtotal = producto.offerPrice * item.cantidad;
-
     const $li = $(`
       <li class="list-group-item mb-3" data-id="${item.id}">
         <div class="d-flex flex-column gap-1">
@@ -44,30 +44,27 @@ function mostrarResumenPedido() {
         </div>
       </li>
     `);
-
     $resumen.append($li);
   });
-
+  //Actualizamos gastos y total
   actualizarGastosYTotal();
 }
-
+//Variables para eliminar elementos con animación
 let itemPendienteEliminar = null;
 let posicionesAntesEliminar = [];
-
+//Función para llamar a la modal previa a eliminar items
 $('#resumen-pedido').on('click', '.btn-eliminar-checkout', function () {
   const $li = $(this).closest('li');
   itemPendienteEliminar = $li.data('id');
   posicionesAntesEliminar = guardarPosiciones();
   mostrarModalConfirmarEliminacion();
 });
-
+//Función para elininar items tras confirmar la modal con animación
 $(document).on('click', '#btn-confirmar-eliminar', function () {
   if (!itemPendienteEliminar) return;
   modalEliminar.hide();
-
   const $liEliminar = $(`#resumen-pedido li[data-id="${itemPendienteEliminar}"]`);
   $liEliminar.addClass('removiendo');
-
   const $contenedor = $('#resumen-pedido').closest('.p-4');
   const alturaAntes = $contenedor.outerHeight();
   const paddingInferiorOriginal = $contenedor.css('padding-bottom');
@@ -125,7 +122,7 @@ $(document).on('click', '#btn-confirmar-eliminar', function () {
     });
   }, 400);
 });
-
+//Función para guardar las posiciones de los elementos que animamos al eliminar items
 function guardarPosiciones() {
   const posiciones = [];
   $('#resumen-pedido li, .IVA').each(function () {
@@ -136,7 +133,7 @@ function guardarPosiciones() {
   });
   return posiciones;
 }
-
+//Función que guarda el carrito en localStorage y backend
 function guardarCarrito() {
   const usuario = localStorage.getItem('usuario') || 'guest';
   localStorage.setItem('carrito', JSON.stringify(carrito));
@@ -150,7 +147,7 @@ function guardarCarrito() {
     error: () => alert('Error al guardar el carrito')
   });
 }
-
+//Función para mostrar el mensaje de carrito vacío
 function mostrarMensajeCarritoVacio() {
   $('#resumen-pedido').append(`
     <li class="list-group-item text-center text-muted border-0 bg-transparent fw-semibold">
@@ -161,7 +158,7 @@ function mostrarMensajeCarritoVacio() {
   $('#envio, #totalSinEnvio, #total').text('0.00 €');
   descuentoAplicado = 0;
 }
-
+//Función para actualizar subtotal, descuento, gastos de envío y total
 function actualizarGastosYTotal() {
   let totalPedido = 0;
   let totalUnidades = 0;
@@ -179,7 +176,6 @@ function actualizarGastosYTotal() {
   let gastosEnvioNormal = totalUnidades > 0 ? 10 + (totalUnidades - 1) * 5 : 0;
   let gastosEnvioFinal = (metodoEnvio === 'tienda' || totalConDescuento > 500) ? 0 : gastosEnvioNormal;
 
-// En vez de solo modificar el contenido visible, añade el valor real en data-envio:
 if (gastosEnvioFinal === 0 && gastosEnvioNormal > 0) {
   $('#envio')
     .html(`
@@ -195,7 +191,6 @@ if (gastosEnvioFinal === 0 && gastosEnvioNormal > 0) {
     .attr('data-envio', gastosEnvioFinal.toFixed(2));
 }
 
-
   if (descuentoAplicado > 0) {
     $('#totalSinEnvio').html(`
       <span style="text-decoration: line-through; color: #888; margin-right: 8px;">
@@ -209,7 +204,7 @@ if (gastosEnvioFinal === 0 && gastosEnvioNormal > 0) {
 
   $('#total').text(`${(totalConDescuento + gastosEnvioFinal).toFixed(2)} €`);
 }
-
+//Función para aplicar cupones de descuento. Los obtenemos del backend con la función aplicarCupon
 $('#btn-aplicar-cupon').on('click', async function () {
   const codigo = $('#input-cupon').val().trim();
   let subtotal = 0;
@@ -236,7 +231,7 @@ $('#btn-aplicar-cupon').on('click', async function () {
 
   actualizarGastosYTotal();
 });
-
+//Función de envío del formulario. Muestra la modal de confirmación previa al pago
 $('#form-checkout').on('submit', function (e) {
   e.preventDefault();
   $('#input-cupon')
@@ -247,7 +242,7 @@ $('#form-checkout').on('submit', function (e) {
    mostrarModalRealizarPago();
 
 });
-
+//Animación de los items del resumen al cargar la página. Carga también los datos de usuario
 $(document).ready(() => {
   //Animacion contenedor al cargar
     $('#contenedor-resumen').addClass('fade-in-smooth');
@@ -267,7 +262,7 @@ $(document).ready(() => {
     }
   }
 
-  // Manejar cambio de método de envío
+  // Manejar cambio de método de envío. Muestra el resto de elementos del envio a domicilio y se actualizan los gastos de envío
   $('#metodoEnvio').on('change', function () {
     const valor = $(this).val();
     metodoEnvio = valor;
@@ -281,9 +276,8 @@ $(document).ready(() => {
     }
 
     actualizarGastosYTotal();
-  }).trigger('change'); // Ejecuta al cargar
+  }).trigger('change');
 
-  // Cargar productos desde el backend
   $.get('/api/products', function (data) {
     productosDisponibles = data;
     mostrarResumenPedido();
@@ -363,7 +357,6 @@ $(document).on('click', '#btnDatosPago', function () {
   const confirmarModal = bootstrap.Modal.getInstance(confirmarModalEl);
   if (confirmarModal) confirmarModal.hide();
 
-  // Abrir modal para introducir datos de pago según método
   mostrarModalDatosPago(metodoSeleccionado);
 });
 
@@ -390,7 +383,8 @@ $(document).on('click', '#btnConfirmarPago', function () {
   if (modalDatos) modalDatos.hide();
 
   if (metodo === 'transferencia') {
-    // No mostrar modal de pago confirmado para transferencia, guardar en backend y borrar todo
+    // No mostrar modal de pago confirmado para transferencia. Guardamos en backend y enviamos a factura
+    //Enviamos al backend, vaciamos el carrito, lo guardamos en el backend para limpiarlo, limpiamos los campos y enviamos a factura
     enviarPedido(obtenerDatosPedido());
     carrito = [];
     guardarCarrito();
@@ -401,7 +395,7 @@ $(document).on('click', '#btnConfirmarPago', function () {
     return;
   }
     if (metodo === 'bizum') {
-    // No mostrar modal de pago confirmado para transferencia, guardar en backend y borrar todo
+    // Igual que transferencia, no mostramos pago confirmado
     enviarPedido(obtenerDatosPedido());
     carrito = [];
     guardarCarrito();
@@ -414,11 +408,11 @@ $(document).on('click', '#btnConfirmarPago', function () {
   }
 
   if (metodoEnvio === 'tienda') {
-    localizador = generarLocalizador(); // Función para generar el código
+    localizador = generarLocalizador(); // Función para generar un localizador
     mensajeEl.innerHTML = `✅ ¡Gracias por tu compra!<br>Ya puedes acudir a nuestra tienda con tu DNI y tu localizador <strong>${localizador}</strong>.`;
   }
 
-  // Mostrar modal de pago confirmado, antes guardamos el pedido
+  // Mostrar modal de pago confirmado, antes guardamos el pedido en backend
   enviarPedido(obtenerDatosPedido(localizador));
   const modalPagoConfirmadoEl = document.getElementById('modalPagoConfirmado');
   let modalPagoConfirmado = bootstrap.Modal.getInstance(modalPagoConfirmadoEl);
@@ -439,7 +433,7 @@ $(document).on('click', '#btnConfirmarPago', function () {
 });
 
 
-//Localizador simulado
+//Función para generar un localizador al azar
 function generarLocalizador() {
   const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let localizador = 'PED-';
@@ -451,24 +445,19 @@ function generarLocalizador() {
 
 //Para limpiar el formulario tras el pago
 function limpiarFormularioPago() {
-  // Limpiar todos los campos de texto
   $('#form-checkout input[type="text"], #form-checkout input[type="email"], #form-checkout input[type="tel"]').val('');
 
-  // Reiniciar selects a su primera opción
   $('#form-checkout select').each(function () {
     $(this).prop('selectedIndex', 0);
   });
 
-  // Ocultar sección de dirección si estaba visible
   $('#datos-envio').hide();
 
-  // Eliminar clases de validación de Bootstrap (si usas)
   $('#form-checkout').removeClass('was-validated');
 
-  // También puedes eliminar manualmente clases de error si las aplicas campo a campo
   $('#form-checkout input, #form-checkout select').removeClass('is-invalid is-valid');
 }
-//Paa deshabilitar boton de pago si el carrito está vacío
+//Funciñon para deshabilitar botón de pago con carrito vacío
 function actualizarEstadoBotonCheckout() {
   const boton = document.getElementById('btnSubmitCheckout');
   
@@ -489,17 +478,19 @@ function mostrarError(inputSelector, mensaje) {
   input.addClass('is-invalid');
   input.next('.invalid-feedback').text(mensaje).show();
 }
-
+//Para limpiar los errores cuando el campo es correcto
 function limpiarError(inputSelector) {
   const input = $(inputSelector);
   input.removeClass('is-invalid');
   input.next('.invalid-feedback').text('').hide();
 }
+//Para limpiar todos los erro
 function limpiarTodosErrores(formSelector) {
   $(`${formSelector} .form-control`).each(function() {
     limpiarError(this);
   });
 }
+//Función para validar campos de modal paypal
 function validarPaypal() {
   limpiarTodosErrores('#formPaypal');
   let valido = true;
@@ -521,6 +512,7 @@ function validarPaypal() {
 
   return valido;
 }
+//Función para validar campos de tarjeta
 function validarTarjeta() {
   limpiarTodosErrores('#formTarjeta');
   let valido = true;
@@ -563,7 +555,7 @@ function validarTarjeta() {
 //Función para enviar al backend los datos del pedido:
 function enviarPedido(pedido) {
 
-    return fetch('/api/orders', {
+  return fetch('/api/orders', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify(pedido)
@@ -574,7 +566,7 @@ function enviarPedido(pedido) {
     return res.json();
   })
   .then(data => {
-  // Guardamos en localStorage para factura.html
+  // Recibimos respuesta del servidor añadiendo el id y la fecha
     localStorage.setItem("ultimoPedido", JSON.stringify({
       id: data.id,
       createdAt: data.createdAt,
@@ -586,7 +578,7 @@ function enviarPedido(pedido) {
   }));
   })
 }
-//Función para recopilar los datos del clientte
+//Función para recopilar los datos del cliente
 function obtenerDatosClienteDesdeFormulario() {
   const nombre = document.getElementById('nombre').value.trim();
   const apellidos = document.getElementById('apellidos').value.trim();
@@ -655,38 +647,25 @@ function obtenerProductosDelResumen() {
 
   return productos;
 }
-
+//Función que reune todos los datos, cliene y pedido, añadimos localizador opcional
 function obtenerDatosPedido(localizador = null) {
-  // Primero obtenemos los datos del cliente (puedes reutilizar tu función)
   const datosCliente = obtenerDatosClienteDesdeFormulario();
-
-  // Obtenemos el array de productos
   const productos = obtenerProductosDelResumen();
-
-  // Leemos el subtotal sin envío (y sin descuento, está tachado si hay descuento)
-  // El valor real sin descuento está en el texto tachado (span anterior al strong)
   let subtotalTexto = "";
   let subtotal = 0;
   const totalSinEnvioEl = document.querySelector("#totalSinEnvio");
   if (totalSinEnvioEl.querySelector("span")) {
-    // Hay descuento: el subtotal original está tachado en el span
     subtotalTexto = totalSinEnvioEl.querySelector("span").textContent;
   } else {
-    // No hay descuento, el subtotal está directamente ahí
     subtotalTexto = totalSinEnvioEl.textContent;
   }
   subtotal = parseFloat(subtotalTexto.replace(/[^\d.,]/g, "").replace(",", "."));
 
-  // Leemos los gastos de envío
   const gastosEnvio = parseFloat(document.querySelector("#envio").getAttribute("data-envio"));
 
-
-  // Leemos el total final
   const totalTexto = document.querySelector("#total").textContent;
   const total = parseFloat(totalTexto.replace(/[^\d.,]/g, "").replace(",", "."));
 
-  // Añadimos el descuentoAplicado (usa la variable global o pásala)
-  // Por seguridad, la obtenemos de la variable global o si no 0
   const descuento = typeof descuentoAplicado === "number" ? descuentoAplicado : 0;
 
   const pedido = {
@@ -703,7 +682,6 @@ function obtenerDatosPedido(localizador = null) {
   if (localizador) {
     pedido.localizador = localizador;
   }
-
     return pedido;
 }
 
